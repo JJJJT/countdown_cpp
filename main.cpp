4f38b7dc2ae326823b8114f870bdf9d89b8345d4
James Kong <k.kong@actimirror.com>
2016-05-24 10:02:32 +0800

    init

---
+++ main.cpp
@@ -0,0 +1,24 @@
+//main.c
+#include <stdio.h>      /* printf */
+#include <time.h>       /* time_t, struct tm, difftime, time, mktime */
+#include <unistd.h>
+int main(){
+
+	time_t timer;
+	
+	time(&timer);
+	struct tm y2k = {0};
+	double seconds;
+
+	y2k.tm_hour = 0;   y2k.tm_min = 0; y2k.tm_sec = 0;
+  	y2k.tm_year = 147; y2k.tm_mon = 6; y2k.tm_mday = 1;
+
+	
+	while(seconds >0){
+		time(&timer);
+		seconds = difftime(mktime(&y2k),timer);
+		printf ("%.f seconds to July 1, 2047 in the current timezone\n", seconds);
+		sleep(1);
+	}
+	return 0;
+}
